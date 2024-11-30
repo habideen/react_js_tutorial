c2e0030dde29fe5df44ac201a5deef2204860335
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-11-30 07:17:30 +0100

    useEffect: delay initial page load

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import Greeting from './components/Greeting';
-import Counter from './components/Counter';
-import Form from './components/Form';
-import CheckAuth from './components/CheckAuth';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Greeting from "./components/Greeting";
+import Counter from "./components/Counter";
+import Form from "./components/Form";
+import CheckAuth from "./components/CheckAuth";
 
-function App() {
+function content() {
   const name = "Habideen Ibrahim";
   const greeting = "Hello, " + name + "!";
 
@@ -14,12 +15,22 @@ function App() {
       <h1>{greeting}</h1>
       <p>Welcome to learning JSX in React.</p>
       <p>The sum of 10 and 20 is {10 + 20}</p>
-      
+
       <Greeting name="Habideen from props" age="23" />
       <Counter />
       <Form />
-    </div>  
+    </div>
   );
+}
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 3000);
+  }, []);
+
+  return <>{isLoading ? <p>Loading...</p> : content()}</>;
 }
 
 export default App;
