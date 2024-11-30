5bcb031e467bfffe114b72d81de162caf4d531cf
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-11-30 07:27:28 +0100

    useEffect: delay initial page load

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setIsLoading(false), 3000);
+    setTimeout(() => setIsLoading(false), 1000);
   }, []);
 
   return <>{isLoading ? <p>Loading...</p> : content()}</>;
