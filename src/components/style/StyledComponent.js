bbcdc7969bda7b05cca8ca070c0c3b6c4f83ebb4
Habideen Ibrahim <habideenibrahim@gmail.com>
2024-12-04 08:54:56 +0100

    styled components: use hover and active event

--- src/components/style/StyledComponent.js
+++ src/components/style/StyledComponent.js
@@ -11,7 +11,9 @@ const Title = styled.h1`
   font-size: 2rem;
 `;
 
-const Button = styled.button`
+const Button = styled.button.attrs(({ $primary }) => ({
+  className: $primary ? "btn-primary" : "btn"
+}))`
   background: transparent;
   border-radius: 3px;
   border: 2px solid #bf4f74;
@@ -19,13 +21,36 @@ const Button = styled.button`
   margin: 0.5em 1em;
   padding: 0.25em 1em;
 
+  &:hover {
+    background: #bf4f74;
+    color: white;
+    cursor: pointer;
+  }
+
+  &:active {
+    background: #a13b5d;
+    border: 2px solid #a13b5d;
+    color: white;
+  }
+
   ${(props) =>
     props.$primary &&
     css`
       background: #bf4f74;
       color: white;
+
+      &:hover {
+        background: #a13b5d;
+        border: 2px solid #a13b5d;
+      }
+
+      &:active {
+        background: #8f3152;
+        border: 2px solid #8f3152;
+      }
     `}
 `;
+
 
 function StyledComponent() {
   return (
